ec35d13be337a00a8abfe1c169e0acde8e422a5d
Timothy Hood <tjhood97@gmail.com>
2019-03-31 18:25:38 -0400

    Added a lot of skeleton and then some
    Created two landing pages, one for regular users and one for admins. Implemented a secure way of keeping admin mode status after restart

---
+++ mu_library_app/mu_library_app/lib/admin_landing_page.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class AdminLandingPage extends StatefulWidget{
+  @override
+  _AdminLandingPageState createState() => _AdminLandingPageState();
+}
+
+class _AdminLandingPageState extends State<AdminLandingPage>{
+
+  final String _adminKey = "admin_mode";
+
+  @override
+  Widget build(BuildContext context){
+    return WillPopScope(
+      onWillPop: ()async => false,
+      child: Scaffold(
+        appBar: AppBar(
+          //TODO: Make this look better, or make it look good without
+          leading: Container(),
+          title: Text("Admin Mode"),
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/tarver.jpeg"),
+              fit: BoxFit.cover,
+              matchTextDirection: true,
+              //Reduce opacity of background image
+              colorFilter: ColorFilter.mode(
+                  Colors.black.withOpacity(0.3),
+                  BlendMode.dstATop
+              ),
+            ),
+          ),
+
+          child: Center(
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text("Welcome, Admin", textScaleFactor: 2.5,),
+
+                  RaisedButton(
+                    child: _buttonText("View All Appointments"),
+                    onPressed: null,
+                  ),
+
+                  RaisedButton(
+                    child: _buttonText("Turn Off Admin Mode"),
+                    onPressed: () => _turnOffAdmin(context),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _turnOffAdmin(BuildContext context) async{
+    final FlutterSecureStorage storage = FlutterSecureStorage();
+    await storage.write(key: _adminKey, value: "false");
+    Navigator.of(context).pop();
+  }
+}
+
+Text _buttonText(String s) {
+  return Text(
+    s,
+    style: TextStyle(
+      fontWeight: FontWeight.bold,
+    ),
+  );
+}
+
---
+++ mu_library_app/mu_library_app/lib/landing_page.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'admin_landing_page.dart';
+
+class LandingPage extends StatefulWidget{
+  @override
+  _LandingPageState createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage>{
+
+  final String _adminKey = "admin_mode";
+
+  @override
+  void initState(){
+    super.initState();
+    _getMode().then((result){
+      if(result)
+        _navToAdminLandingPage();
+    });
+  }
+
+  Future<bool> _getMode() async{
+    final FlutterSecureStorage storage = FlutterSecureStorage();
+    String _admin = await storage.read(key: _adminKey);
+    if(_admin != null || _admin == "true")
+      return true;
+    else
+      return false;
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/tarver.jpeg"),
+            fit: BoxFit.cover,
+            matchTextDirection: true,
+            //Reduce opacity of background image
+            colorFilter: ColorFilter.mode(
+                Colors.black.withOpacity(0.3),
+                BlendMode.dstATop
+            ),
+          ),
+        ),
+
+        child: Center(
+          //TODO: Make this look better
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text("Welcome", textScaleFactor: 3.0,),
+
+                RaisedButton(
+                  child: _buttonText("Book an Appointment"),
+                  onPressed: null,
+                ),
+
+                RaisedButton(
+                    child: _buttonText("Turn On Admin Mode"),
+                    onPressed: _turnOnAdmin,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Text _buttonText(String s) {
+    return Text(
+      s,
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+
+  void _navToAdminLandingPage(){
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => AdminLandingPage())
+    );
+  }
+
+  void _turnOnAdmin() async{
+    final FlutterSecureStorage storage = FlutterSecureStorage();
+    await storage.write(key: _adminKey, value: "true");
+    _navToAdminLandingPage();
+  }
+}
+
