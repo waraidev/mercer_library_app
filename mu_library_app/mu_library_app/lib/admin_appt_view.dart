3bc74c3783eccb2bdc801922c5e7e4544e822606
waraidev <wileye92@gmail.com>
2019-04-22 18:14:52 -0400

    More work on Admin View Appts
    The Admin View Appointments page now kind of works, but there are still some errors that need to be worked out.

--- mu_library_app/mu_library_app/lib/admin_appt_view.dart
+++ mu_library_app/mu_library_app/lib/admin_appt_view.dart
@@ -2,22 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'fire_model.dart';
 
-class ViewAdminAppt extends StatelessWidget {
+class ViewAdminAppt extends StatefulWidget {
+  @override
+  createState() => _ViewAdminState();
+}
+
+class _ViewAdminState extends State<ViewAdminAppt> {
 
   final mainReference = Firestore.instance.collection('appointments');
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: mainReference.snapshots(),
-      builder: (context, snapshot) {
-        if(!snapshot.hasData) return CircularProgressIndicator();
+    return Scaffold(
+      appBar: AppBar(title: Text('Admin View Appointments')),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: mainReference.snapshots(),
+        builder: (context, snapshot) {
+          if(!snapshot.hasData) return CircularProgressIndicator();
 
-        List<DocumentSnapshot> docList = snapshot.data.documents;
-        return ListView.builder(
-          itemBuilder: null,
-          itemCount: docList.length,
-        );
-      },
+          List<DocumentSnapshot> docList = snapshot.data.documents;
+
+          return ListView.builder(
+            itemBuilder: (context, index) =>
+                _buildAdminList(docList, context, index),
+            itemCount: docList.length,
+          );
+        },
+      ),
     );
   }
 
@@ -25,7 +35,19 @@ class ViewAdminAppt extends StatelessWidget {
         BuildContext context, int index) {
     DocumentSnapshot ds = list[index];
 
-    return null;  //Finish this later
+    return ExpansionTile(
+      key: PageStorageKey<int>(0),
+      title: Text(ds['name'] + ', ' + ds['datetime'].toDate().toString()),
+      children: <Widget>[
+        Text(ds['name'] + ', ' + ds['muid']),
+        Text(ds['email']),
+        Text(ds['major']),
+        Text(ds['datetime'].toDate().toString()),
+        Text(ds['location'] + ', ' + ds['specloc']),
+        Text(ds['meeting-type']),
+        Text(ds['details'])
+      ],
+    );
 
     //Add expansion panels.
   }
