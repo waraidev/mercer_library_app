c30bfdf7461f9c369c9b09996b1d32820a9d3223
waraidev <wileye92@gmail.com>
2019-04-22 21:27:08 -0400

    Finished admin view appts and made Details textfield larger
    I finished the admin view appointments page with expandable tiles and bold text to highlight important info.

--- mu_library_app/mu_library_app/lib/admin_appt_view.dart
+++ mu_library_app/mu_library_app/lib/admin_appt_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'fire_model.dart';
+import 'package:intl/intl.dart';
 
 class ViewAdminAppt extends StatefulWidget {
   @override
@@ -35,17 +35,106 @@ class _ViewAdminState extends State<ViewAdminAppt> {
         BuildContext context, int index) {
     DocumentSnapshot ds = list[index];
 
+    var dateTime = DateFormat("MMM dd, yyyy 'at' hh:mm a")
+        .format(ds['datetime'].toDate());
+
     return ExpansionTile(
       key: PageStorageKey<int>(0),
-      title: Text(ds['name'] + ', ' + ds['datetime'].toDate().toString()),
+      title: Text(ds['name'] + '  |  ' + dateTime),
       children: <Widget>[
-        Text(ds['name'] + ', ' + ds['muid']),
-        Text(ds['email']),
-        Text(ds['major']),
-        Text(ds['datetime'].toDate().toString()),
-        Text(ds['location'] + ', ' + ds['specloc']),
-        Text(ds['meeting-type']),
-        Text(ds['details'])
+        Padding(
+          padding: EdgeInsets.all(15.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              RichText(   //Name and MUID
+                text: TextSpan(
+                    style: TextStyle(       //Rich Text allows the first part to
+                        color: Colors.black           //be bold
+                    ),
+                    children: <TextSpan>[
+                      TextSpan(text: 'Name, MUID: ',
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                      TextSpan(text: ds['name'] + ', ' + ds['muid'])
+                    ]
+                ),
+              ),
+              RichText(     //Email
+                text: TextSpan(
+                    style: TextStyle(
+                        color: Colors.black
+                    ),
+                    children: <TextSpan>[
+                      TextSpan(text: 'Email: ',
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                      TextSpan(text: ds['email'])
+                    ]
+                ),
+              ),
+              RichText(     //Major
+                text: TextSpan(
+                    style: TextStyle(
+                        color: Colors.black
+                    ),
+                    children: <TextSpan>[
+                      TextSpan(text: 'Major: ',
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                      TextSpan(text: ds['major'])
+                    ]
+                ),
+              ),
+              RichText(     //Date and Time
+                text: TextSpan(
+                    style: TextStyle(
+                        color: Colors.black
+                    ),
+                    children: <TextSpan>[
+                      TextSpan(text: 'Date and Time: ',
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                      TextSpan(text: dateTime)
+                    ]
+                ),
+              ),
+              RichText(     //Location
+                text: TextSpan(
+                    style: TextStyle(
+                        color: Colors.black
+                    ),
+                    children: <TextSpan>[
+                      TextSpan(text: 'Location: ',
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                      TextSpan(text: ds['location'] + ', ' + ds['specloc'])
+                    ]
+                ),
+              ),
+              RichText(     //Meeting Type
+                text: TextSpan(
+                    style: TextStyle(
+                        color: Colors.black
+                    ),
+                    children: <TextSpan>[
+                      TextSpan(text: 'Meeting Type: ',
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                      TextSpan(text: ds['meeting-type'])
+                    ]
+                ),
+              ),
+              RichText(     //Details
+                text: TextSpan(
+                    style: TextStyle(
+                        color: Colors.black
+                    ),
+                    children: <TextSpan>[
+                      TextSpan(text: 'Details: ',
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                      TextSpan(text: ds['details'])
+                    ]
+                ),
+              ),
+              SizedBox(height: 14)
+            ],
+          ),
+        )
       ],
     );
 
